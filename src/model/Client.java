a4f61db30fd65eea68fccc8c73504721b2fc8b4b
Mp6 <matteo.pulcini19@gmail.com>
2021-12-26 16:42:42 -0500

    Client and Server Tempalte
    Client and Server Template. Credit to  www.codejava.net.

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,30 +1,58 @@
 package model;
 
-import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import java.net.*;
+import java.io.*;
+ 
+/**
+ * This program demonstrates a simple TCP/IP socket client that reads input
+ * from the user and prints echoed message from the server.
+ *
+ * @author www.codejava.net
+ */
 public class Client {
-	public static void main(String[] args) throws Exception {
-		/*
-		 *  Identify side using port number
-		 */
-		
-		String ip = "localhost";
-		int port = 9999;
-		Socket s = new Socket(ip, port);
-		
-		String str = "Name";
-		
-		/*
-		 * Output Steam Writer - Converts data into stream format 
-		 * Data is being sent to i.e., socket, printer, monitor
-		 * getOutputStream from socket
-		 */
-		OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream()); 
-		PrintWriter out = new PrintWriter(os);
-		
-		os.write(str);
-		os.flush();
-	}
-}  
+ 
+    public static void main(String[] args) {
+        if (args.length < 2) return;
+ 
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+ 
+        try (Socket socket = new Socket(hostname, port)) {
+ 
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+ 
+            Console console = System.console();
+            String text;
+ 
+            do {
+                text = console.readLine("Enter text: ");
+ 
+                writer.println(text);
+ 
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+                String time = reader.readLine();
+ 
+                System.out.println(time);
+ 
+            } while (!text.equals("bye"));
+ 
+            socket.close();
+ 
+        } catch (UnknownHostException ex) {
+ 
+            System.out.println("Server not found: " + ex.getMessage());
+ 
+        } catch (IOException ex) {
+ 
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
--- src/model/Server.java
+++ src/model/Server.java
@@ -1,32 +1,55 @@
 package model;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
-
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+ 
+/**
+ * This program demonstrates a simple TCP/IP socket server that echoes every
+ * message from the client in reversed form.
+ * This server is single-threaded.
+ *
+ * @author www.codejava.net
+ */
 public class Server {
-
-	public static void main(String[] args) throws Exception {
-		System.out.println("Server is started...");
-		ServerSocket ss = new ServerSocket(9999); //check port number interacted with by the client
-		
-		System.out.println("Server is waiting for client request...");
-		Socket s = ss.accept();
-		
-		System.out.println("Client Connected");
-		
-		/* fetch the data */
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		String str = br.readLine();
-		
-		
-		System.out.println("Client Data: " + str);
-		
-		
-		
-
-	}
-
+ 
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+ 
+        int port = Integer.parseInt(args[0]);
+        int index = 0;
+        ArrayList<String> a = new ArrayList<String>();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+ 
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+ 
+                String text;
+ 
+                do {
+                    text = reader.readLine();
+                   
+                    a.add(text);
+                    writer.println("Server (Blocked Site): " + text);
+                    writer.println(a.get(index));
+                    index++;
+                } while (!text.equals("bye"));
+ 
+                socket.close();
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 }
